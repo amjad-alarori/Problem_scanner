490303cbeefc65021118275e1bbd68c41b2763f7
“SamirMokiem” <samir@mokiem.nl>
2021-05-21 14:06:41 +0200

    fixes

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -80,27 +80,5 @@ class UsersTableSeeder extends Seeder
             $newUser;
             $newUser->attachRole($userRole);
         }
-
-        $faker = Factory::create();
-
-        $scan = Scan::create([
-            'name' => 'Scan'
-        ]);
-
-        for ($int = 0; $int < 10; $int++) {
-            $cat = Categories::create([
-                'name' => $faker->jobTitle,
-                'scan_id' => $scan->id,
-                'color' => '#000000',
-                'image' => 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'
-            ]);
-            for ($intt = 0; $intt < 10; $intt++) {
-                Questions::create([
-                    'question' => $faker->company,
-                    'categories_id' => $cat->id,
-                    'image' => 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'
-                ]);
-            }
-        }
     }
 }
